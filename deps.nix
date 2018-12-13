13a0077adf14fe39751d08c64e836905187dff81
Dustin Lacewell <dlacewell@gmail.com>
2018-12-13 01:43:01 -0600

    Update t3sketch

--- deps.nix
+++ deps.nix
@@ -23,8 +23,8 @@ with import <nixpkgs> {};
     src = fetchFromGitHub {
       owner = "dustinlacewell";
       repo = "t3sketch";
-      rev = "2116be5564713f4dbf290054e9ebfba3a591a54e";
-      sha256 = "1w24z10incipz4j9yxn900g46m584np675cfvswgacniyh1ww2bs";
+      rev = "71d20cb6df561c4f63e059841fa9487cb2f2777f";
+      sha256 = "1lrv5l2r5wsbvcx6l0679z1ds3kil7szp52ni4x2kc4qbx26n1rr";
     };
   };
 }
